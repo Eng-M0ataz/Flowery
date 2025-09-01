9ec7726657522271fffe57d3493fb0e86d938e1f
YassenAhmed-gif <yassentest5@gmail.com>
2025-09-02 01:06:30 +0300

    Refactor: Update Best Seller Feature
    This commit refactors the Best Seller feature, including UI adjustments and data model updates.
    
    Key changes:
    - Updated `BestSellerDto` to include new fields like `discount` and reordered existing fields. Removed the `toEntity` method.
    - Modified `BestSellerEntity` to include additional properties such as `slug`, `images`, `discount`, `rateAvg`, `rateCount`, `quantity`, `category`, `occasion`, `isSuperAdmin`, `createdAt`, `updatedAt`, `V`, `sold`, and `id`. The `discountPercent` getter was removed.
    - Updated the `toEntity` extension method in `best_seller_dto_mapper.dart` to map all new fields from `BestSellerDto` to `BestSellerEntity`.
    - Added an import for `best_seller_dto_mapper.dart` in `best_seller_response_dto_mapper.dart`.
    - In `best_seller_screen.dart`:
        - Implemented price calculation logic for products with and without discounts.
        - Replaced `GridView.builder` with `ResponsiveGridView.builder` for better layout adaptability.
        - Adjusted `childAspectRatio` and related grid delegate properties.
    - In `custom_product_card.dart`:
        - Adjusted padding, container height, and clip behavior for the product image.
        - Modified the layout of product details (title, price, discount) for better presentation.
        - Made minor adjustments to spacing.
    - Added a new size constant `sizedBoxHeight_25` in `sizes.dart`.

--- lib/core/Config/Routing/route_generator.dart
+++ lib/core/Config/Routing/route_generator.dart
@@ -1,9 +1,8 @@
-import 'package:flower_e_commerce_app/Feature/auth/presentation/pages/forget_password.dart';
+// import 'package:flower_e_commerce_app/Feature/auth/presentation/pages/forget_password.dart';
 import 'package:flower_e_commerce_app/Feature/auth/presentation/pages/sign_in_screen.dart';
-import 'package:flower_e_commerce_app/Feature/auth/presentation/pages/sign_up_screen.dart';
+// import 'package:flower_e_commerce_app/Feature/auth/presentation/pages/sign_up_screen.dart';
 import 'package:flower_e_commerce_app/Feature/mainLayout/main_layout.dart';
 import 'package:flower_e_commerce_app/core/Utils/constants/app_routes.dart';
-import 'package:flower_e_commerce_app/core/helpers/routing_extensions.dart';
 import 'package:flutter/material.dart';
 
 class RouteGenerator {
@@ -12,9 +11,9 @@ class RouteGenerator {
       case AppRoutes.signInRoute:
         return MaterialPageRoute(builder: (_) => const SigninScreen());
       case AppRoutes.signUpRoute:
-        return MaterialPageRoute(builder: (_) => const SignUpScreen());
+        // return MaterialPageRoute(builder: (_) => const SignUpScreen());
       case AppRoutes.forgetPasswordRoute:
-        return MaterialPageRoute(builder: (_) => const ForgetPassword());
+        // return MaterialPageRoute(builder: (_) => const ForgetPassword());
       case AppRoutes.mainLayoutRoute:
         return MaterialPageRoute(builder: (_) => const MainLayout());
       default:
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flower_e_commerce_app/core/Config/Routing/route_generator.dart';
 import 'package:flower_e_commerce_app/core/Config/Theme/app_theme.dart';
 import 'package:flower_e_commerce_app/core/Di/di.dart';
+import 'package:flower_e_commerce_app/core/Services/storage_interface.dart';
 import 'package:flower_e_commerce_app/core/Utils/constants/app_routes.dart';
 import 'package:flower_e_commerce_app/core/helpers/block_observer.dart';
 import 'package:flower_e_commerce_app/core/utils/Constants/app_constants.dart';
@@ -9,8 +10,6 @@ import 'package:flower_e_commerce_app/core/utils/Constants/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:responsive_framework/responsive_framework.dart';
-
-import 'core/Services/storge_interface.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -57,7 +56,7 @@ class FlowerECommerceApp extends StatelessWidget {
         breakpointsLandscape: AppSizes.appLandscapeBreakPoints,
       ),
       theme: AppThemeLight.lightTheme,
-      initialRoute: initialRoute, // ✅ FIXED HERE
+      initialRoute: initialRoute,
       onGenerateRoute: RouteGenerator.getRoute,
     );
   }
