f0fba60811254b8d8e2b52b9086673547cb3ad5b
Eng-Moataz <moatazebrahim221@gmail.com>
2025-09-18 02:57:33 +0300

    Fix : Search api service in import api_constants

--- lib/Feature/searchFeature/api/client/search_api_service.dart
+++ lib/Feature/searchFeature/api/client/search_api_service.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flower_e_commerce_app/Feature/searchFeature/api/models/search_response_dto.dart';
-import 'package:flower_e_commerce_app/core/utils/constants/api_constants.dart';
+import 'package:flower_e_commerce_app/core/utils/Constants/api_constants.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 part 'search_api_service.g.dart';
