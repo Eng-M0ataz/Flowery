349dbbd2fd0c2deb2e128718f61983a004fd5370
Eng-Moataz <moatazebrahim221@gmail.com>
2025-10-28 17:37:15 +0300

    refactor: update best seller screen layout and enhance UI components
    - Replaced AppBar with a custom back button for improved navigation.
    - Updated the body layout to use a Column widget for better organization.
    - Introduced ProductsShimmer for loading state representation.
    - Adjusted padding and text styles for better visual consistency.

--- lib/Feature/bestSellerFeature/presentation/pages/best_seller_screen.dart
+++ lib/Feature/bestSellerFeature/presentation/pages/best_seller_screen.dart
@@ -1,15 +1,17 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flower_e_commerce_app/Feature/bestSellerFeature/presentation/viewModel/best_seller_state.dart';
+import 'package:flower_e_commerce_app/core/Config/Theme/app_colors.dart';
 import 'package:flower_e_commerce_app/core/Di/di.dart';
 import 'package:flower_e_commerce_app/core/Functions/snack_bar.dart';
+import 'package:flower_e_commerce_app/core/Widgets/custom_app_bar.dart';
+import 'package:flower_e_commerce_app/core/Widgets/products_shimmer.dart';
 import 'package:flower_e_commerce_app/core/helpers/dialogue_utils.dart';
-import 'package:flower_e_commerce_app/core/helpers/routing_extensions.dart';
 import 'package:flower_e_commerce_app/core/localization/locale_keys.g.dart';
 import 'package:flower_e_commerce_app/core/utils/Constantts/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../../../../core/Widgets/product_card.dart';
-import '../../../mainLayout/tabs/home/presentation/widgets/homeWidgets/list_views_shimmer_widget.dart';
 import '../viewModel/best_seller_event.dart';
 import '../viewModel/best_seller_model_view.dart';
 
@@ -68,56 +70,72 @@ class _BestSellerScreenState extends State<BestSellerScreen> {
         },
         builder: (context, state) {
           return Scaffold(
-            appBar: AppBar(
-              leading: GestureDetector(
-                onTap: context.pop,
-                child: const Icon(Icons.arrow_back_ios),
-              ),
-              title: Text(LocaleKeys.best_seller.tr()),
-            ),
-            body: Padding(
-              padding: const EdgeInsets.all(AppSizes.paddingMd_12),
-              child: state.isLoading
-                  ? ListViewsShimmerWidget(isCategorie: false)
-                  : state.bestSellers == null
-                      ? const Center(child: Text('No products found'))
-                      : GridView.builder(
-                          physics: const BouncingScrollPhysics(),
-                          cacheExtent: 500,
-                          gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            crossAxisSpacing: AppSizes.spaceBetweenItems_16,
-                            mainAxisSpacing: AppSizes.spaceBetweenItems_16,
-                            childAspectRatio: 0.68,
-                          ),
-                          itemCount: state.bestSellers!.bestSeller!.length,
-                          itemBuilder: (context, index) {
-                            final product =
-                                state.bestSellers!.bestSeller![index];
-                            final bool isThisCardLoading =
-                                state.selectedBestSellerId == product.Id;
+            appBar: CustomBackButton(title: LocaleKeys.best_seller.tr()),
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 36),
+                  child: Text(LocaleKeys.best_sellers.tr(),
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: AppColorsLight.grey,
+                            fontSize: AppSizes.mdFont_16,
+                          )),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(AppSizes.paddingMd_12),
+                    child: state.isLoading
+                        ? ProductsShimmer(
+                            isEnabled: true,
+                          )
+                        : state.bestSellers == null
+                            ? const Center(child: Text('No products found'))
+                            : GridView.builder(
+                                physics: const BouncingScrollPhysics(),
+                                cacheExtent: 500,
+                                gridDelegate:
+                                    const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2,
+                                  crossAxisSpacing:
+                                      AppSizes.spaceBetweenItems_16,
+                                  mainAxisSpacing:
+                                      AppSizes.spaceBetweenItems_16,
+                                  childAspectRatio: 0.68,
+                                ),
+                                itemCount:
+                                    state.bestSellers!.bestSeller!.length,
+                                itemBuilder: (context, index) {
+                                  final product =
+                                      state.bestSellers!.bestSeller![index];
+                                  final bool isThisCardLoading =
+                                      state.selectedBestSellerId == product.Id;
 
-                            return ProductCard(
-                              imgCover: product.imgCover ?? '',
-                              title: product.title ?? '[Title]',
-                              price: product.price ?? 0,
-                              priceAfterDiscount:
-                                  product.priceAfterDiscount ?? 0,
-                              discountPercent: product.discountPercent,
-                              isAddingToCart: isThisCardLoading,
-                              onAddToCart: () {
-                                if (product.Id != null) {
-                                  context.read<BestSellerViewModel>().doIntent(
-                                        AddProductToCartEvent(
-                                          productId: product.Id!,
-                                        ),
-                                      );
-                                }
-                              },
-                            );
-                          },
-                        ),
+                                  return ProductCard(
+                                    imgCover: product.imgCover ?? '',
+                                    title: product.title ?? '[Title]',
+                                    price: product.price ?? 0,
+                                    priceAfterDiscount:
+                                        product.priceAfterDiscount ?? 0,
+                                    discountPercent: product.discountPercent,
+                                    isAddingToCart: isThisCardLoading,
+                                    onAddToCart: () {
+                                      if (product.Id != null) {
+                                        context
+                                            .read<BestSellerViewModel>()
+                                            .doIntent(
+                                              AddProductToCartEvent(
+                                                productId: product.Id!,
+                                              ),
+                                            );
+                                      }
+                                    },
+                                  );
+                                },
+                              ),
+                  ),
+                ),
+              ],
             ),
           );
         },
